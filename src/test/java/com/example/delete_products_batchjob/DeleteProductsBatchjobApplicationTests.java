5c5aa4ce9eedaba06a6b9d103753812734c68e11
Brandon Tan <brandontan2003@gmail.com>
2025-01-11 23:26:59 +0800

    Update SpringBootTest

--- src/test/java/com/example/delete_products_batchjob/DeleteProductsBatchjobApplicationTests.java
+++ src/test/java/com/example/delete_products_batchjob/DeleteProductsBatchjobApplicationTests.java
@@ -1,10 +1,11 @@
 package com.example.delete_products_batchjob;
 
+import com.example.delete_products_batchjob.config.FlywayTestConfig;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class DeleteProductsBatchjobApplicationTests {
+@SpringBootTest(classes = DeleteProductsBatchjobApplicationTests.class)
+class DeleteProductsBatchjobApplicationTests extends FlywayTestConfig {
 
 	@Test
 	void contextLoads() {
---
+++ src/test/resources/testdb-migration/V1.2__create_staging_table.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS staging_table (
+    staging_id VARCHAR(36) PRIMARY KEY,
+    product_id VARCHAR(36) NOT NULL,
+    status VARCHAR(15),
+    scheduled_deletion_date DATE NOT NULL,
+    completion_date DATE
+);

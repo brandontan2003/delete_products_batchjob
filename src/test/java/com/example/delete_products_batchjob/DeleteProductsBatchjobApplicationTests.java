6884248bf3c1fc9d5694c3f0252e4f0bdf765723
Brandon Tan <brandontan2003@gmail.com>
2025-01-12 14:45:38 +0800

    Update unit test

--- src/test/java/com/example/delete_products_batchjob/DeleteProductsBatchjobApplicationTests.java
+++ src/test/java/com/example/delete_products_batchjob/DeleteProductsBatchjobApplicationTests.java
@@ -1,11 +1,10 @@
 package com.example.delete_products_batchjob;
 
-import com.example.delete_products_batchjob.config.FlywayTestConfig;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest(classes = DeleteProductsBatchjobApplicationTests.class)
-class DeleteProductsBatchjobApplicationTests extends FlywayTestConfig {
+class DeleteProductsBatchjobApplicationTests {
 
 	@Test
 	void contextLoads() {
